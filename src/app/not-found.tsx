073c3cc3dfe0750369cafde71b2973b0ac3b723c
Ben Winding <hello@benwinding.com>
2023-11-05 17:43:38 +1100

    Add custom 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="text-center py-20">
+      <h1>Sorry page not found</h1>
+    </div>
+  );
+};
+
+export default Page;

6fe821dd5c296a6a087f927a79d78df723159651
Ben Winding <hello@benwinding.com>
2023-11-05 17:29:43 +1100

    Add links to Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,24 @@
 import { InternalLink } from "./InternalLink";
 
+const links = [
+  ["/front", "front"],
+  ["/ask", "ask"],
+  ["/show", "show"],
+  ["/about", "about"],
+];
+
 export function Footer() {
   return (
-    <div className="text-gray-600 italic flex justify-center py-8">
-      <InternalLink href="/about">YCombinato</InternalLink>
+    <div className="text-gray-600 flex flex-col items-center py-8">
+      <div className="flex justify-center pb-5">
+        {links.map(([path, title], index) => (
+          <span key={path}>
+            {index !== 0 && <span className="px-3"> | </span>}
+            <InternalLink href={path}>{title}</InternalLink>
+          </span>
+        ))}
+      </div>
+      <InternalLink href="/">YCombinato</InternalLink>
     </div>
   );
 }
